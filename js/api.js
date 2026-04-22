a75194294e9740e2193cc1fc02f86d624853bce5
⚡️T.taoTRM ⚡️ <phitnakha270603@gmail.com>
2026-04-22 13:39:00 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -4,7 +4,7 @@
  */
 
 // ใช้ URL เดียวกับที่ Frontend ใช้ตามที่คุณให้อ่าน
-const API_BASE_URL = "https://rolls-corners-cuisine-covering.trycloudflare.com/api";
+const API_BASE_URL = "http://servertnk-nakhaworking.click/api";
 const BACKEND_URL = API_BASE_URL.replace(/\/api$/, "");
 
 // กำหนดค่าให้ Global
